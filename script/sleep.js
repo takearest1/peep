6330134390414f5d505487b7861fe03e34c36e87
takearest1 <92911648+takearest1@users.noreply.github.com>
2021-11-08 16:42:10 +0900

    Add files via upload

---
+++ script/sleep.js
@@ -0,0 +1,23 @@
+/*
+ベッドで寝る・ベッドの布団の撤去・部屋の証明オフ
+*/
+
+var sleep_f = false;
+//var yoru = document.getElementsByClassName("dark");
+
+function sleep(){
+
+  if(sleep_f == false){
+    syoumei_off();
+
+    $("#futon").css('opacity', '0');
+    chara.innerHTML = '<img id="sleep" class="dark character" src="images/sleep.gif">';
+    $('.dark').css('filter','brightness(' + dark + '%)');
+    sleep_f = true;
+    s_flag = true;
+    console.log('sleeping');
+  }
+  //
+}
+
+//<img id="sleep" class="dark" src="images/sleep.gif">//これを５行目に入れる
---
+++ script/sumaho.js
@@ -0,0 +1,12 @@
+/*
+ベッドでスマホいじる
+*/
+
+var sumaho_f = false;
+
+function sumaho(){
+  if(sumaho_f == false){
+    chara.innerHTML = '<img id="sumaho" class="dark character" src="images/sumaho.gif">';
+    sumaho_f = true;
+  }
+}
