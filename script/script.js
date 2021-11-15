5e9f14defcb25194315d26c22939bbd4f6831834
takearest1 <92911648+takearest1@users.noreply.github.com>
2021-11-15 19:02:54 +0900

    Update script.js

--- script/script.js
+++ script/script.js
@@ -7,22 +7,23 @@ var random = Math.floor(Math.random()* (7 - 1) + 1);
 //var random = 5;
 var t, th, tm;
 //var w = 1;
+var nowtime , previoustime;
 
 window.onload = function(){
-  var walk = setInterval(AnimateElement, 30);//「5」毎にAnimateElementを読み込む。数字を大きくすれば遅くなる。
-  /*$("html,body").animate({scrollLeft:($('#image').width() - $('#hoge').width())/2});
-  $("html,body").animate({scrollTop:($('#container').height() - $('#hoge').height())/2});*/
-  //$("html,body").animate({scrollLeft:($('#chara').offset().left});
+  setInterval(time, 30);//「30」毎にAnimateElementを読み込む。数字を大きくすれば遅くなる。
+
+  animation();
+  haikei();
 
   setTimeout(function () {
     $("html,body").animate({scrollTop:($('.character').offset().top + $('.character').height()/3 - $('#hoge').width()/2)},1000);
     $("html,body").animate({scrollLeft:($('.character').offset().left + $('.character').width()/2 - $('#hoge').width()/2)},1000);
     console.log($('.character').offset());
+
   },300);
 }
 
-function AnimateElement(){
-
+function time(){
   $('.dark').css('filter','brightness(' + dark + '%)');
 
   /////リロード
@@ -37,12 +38,22 @@ function AnimateElement(){
   tm = t.getMinutes();
 
   //th = 19;
-
-  haikei();
   clock();
+  haikei();
 
+  nowtime = th;
+  if(!nowtime == previoustime){
+
+  }
+  previoustime = nowtime;
+
+
+}
+
+function animation() {
   //////男の子の表示
   //random = 4;
+  console.log(random);
   var chara = document.getElementById('chara');
   if(th < 6){
     random = 6;
@@ -50,24 +61,17 @@ function AnimateElement(){
   }else{
     if(random == 1){
       random = Math.floor(Math.random()* (7 - 1) + 1);
+      animation();
     }else if(random == 2){
       pc();
     }else if(random == 3){
       read();
     }else if(random == 4){
-      game();
+      game_syugei();
     }else if(random == 5){
       sumaho();
     }else if(random == 6){
       sleep();
     }
   }
-
-  /*if(window.innerWidth >= 960){
-    w = 1.2;
-  }else {
-    w = 1;
-  }*/
-
-
 }
